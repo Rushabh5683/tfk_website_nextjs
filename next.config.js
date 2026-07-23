f1cdb9cda8ab00e9c74d50ff40ec1ee270b9fba0
rushabh-scalefull <rushabhmahangare.scalefull@gmail.com>
2026-07-23 23:24:29 +0530

    Image Optimisation

--- next.config.js
+++ next.config.js
@@ -8,6 +8,18 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
   },
   compress: true,
   poweredByHeader: false,
